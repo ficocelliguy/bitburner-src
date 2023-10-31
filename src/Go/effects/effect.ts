2329a004f46dedba4c38c78ff2ba630935a88f0e
Michael Ficocelli <ficocemt@gmail.com>
2023-10-31 10:30:53 -0400

    update player mults, fix chain calc bug

--- src/Go/effects/effect.ts
+++ src/Go/effects/effect.ts
@@ -2,6 +2,7 @@ import { currentNodeMults } from "../../BitNode/BitNodeMultipliers";
 import { opponentDetails, opponents } from "../boardState/goConstants";
 import { floor } from "../boardState/boardState";
 import { Player } from "@player";
+import { defaultMultipliers, mergeMultipliers, Multipliers } from "../../PersonObjects/Multipliers";
 
 /**
  * Calculates the effect size of the given player boost, based on the node power (points based on number of subnet
@@ -12,11 +13,58 @@ export function CalculateEffect(nodes: number, faction: opponents): number {
   return 1 + (Math.log(nodes + 1) / 200) * Math.pow((nodes + 1) / 3, 0.2) * power * currentNodeMults.GoPower;
 }
 
+/**
+ * Gets a formatted description of the current bonus from this faction
+ */
 export function getBonusText(opponent: opponents) {
   const nodePower = Player.go.status[opponent].nodePower;
   const effectSize = formatPercent(CalculateEffect(nodePower, opponent));
   const effectDescription = getEffectTypeForFaction(opponent);
   return `${effectSize > 0 ? "+" : ""}${effectSize}% ${effectDescription}?`;
+}
+
+/**
+ * Update the player object, using the multipliers gained from node power for each faction
+ */
+export function updateGoMults(): void {
+  const mults = calculateMults();
+  Player.mults = mergeMultipliers(Player.mults, mults);
+  Player.updateSkillLevels();
+}
+
+/**
+ * Creates a multiplier object based on the player's total node power for each faction
+ */
+function calculateMults(): Multipliers {
+  const mults = defaultMultipliers();
+  [
+    opponents.Netburners,
+    opponents.SlumSnakes,
+    opponents.TheBlackHand,
+    opponents.Daedalus,
+    opponents.Illuminati,
+  ].forEach((opponent) => {
+    const effect = CalculateEffect(Player.go.status[opponent].nodePower, opponent);
+    switch (opponent) {
+      case opponents.Netburners:
+        mults.hacknet_node_purchase_cost *= effect;
+        break;
+      case opponents.SlumSnakes:
+        mults.crime_money *= effect;
+        break;
+      case opponents.TheBlackHand:
+        mults.hacking_exp *= effect;
+        break;
+      case opponents.Daedalus:
+        mults.company_rep *= effect;
+        mults.faction_rep *= effect;
+        break;
+      case opponents.Illuminati:
+        mults.hacking_grow *= effect;
+        break;
+    }
+  });
+  return mults;
 }
 
 function formatPercent(n: number) {
