d75759672ca368c97299c27af4dec70628cdf224
Michael Ficocelli <michael.ficocelli@atomicobject.com>
2026-03-20 22:59:42 -0400

    type fixes

--- src/utils/python/language-service.d.ts
+++ src/utils/python/language-service.d.ts
@@ -18,7 +18,7 @@ export interface RapydScriptOptions {
   stdlibFiles?: Record<string, string>;
   dtsFiles?: Array<{ name: string; content: string }>;
   parseDelay?: number;
-  extraBuiltins?: Record<string, true>;
+  extraBuiltins?: Record<string, boolean>;
   pythonFlags?: string;
   pythonize_strings?: boolean;
   loadDts?: (name: string) => string | Promise<string>;
